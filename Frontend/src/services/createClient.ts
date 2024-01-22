af1e0d58a18823aa4e92f32c01abc4d8c045102f
Vitor <vitorturqueti@hotmail.com>
2024-01-22 08:32:24 -0300

    added try catch to create and update client functions

--- Frontend/src/services/createClient.ts
+++ Frontend/src/services/createClient.ts
@@ -1,12 +1,18 @@
 export const createClient = async (body: any) => {
-    const requestOptions = {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(body)
+    try {
+        const requestOptions = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(body)
+        }
+        const response = await fetch(
+            `${import.meta.env.VITE_BASEURL}:${import.meta.env.VITE_PORT}/clients`,
+            requestOptions
+        )
+        return response.json()
+    } catch {
+        return {
+            error: "Algo deu errado!"
+        }
     }
-    const response = await fetch(
-        `${import.meta.env.VITE_BASEURL}:${import.meta.env.VITE_PORT}/clients`,
-        requestOptions
-    )
-    return response.json()
 }
--- Frontend/src/services/updateClient.ts
+++ Frontend/src/services/updateClient.ts
@@ -1,12 +1,18 @@
 export const updateClient = async (body: any) => {
-    const requestOptions = {
-        method: "PUT",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(body)
+    try {
+        const requestOptions = {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(body)
+        }
+        const response = await fetch(
+            `${import.meta.env.VITE_BASEURL}:${import.meta.env.VITE_PORT}/clients/${body.id}`,
+            requestOptions
+        )
+        return response.json()
+    } catch {
+        return {
+            error: "Algo deu errado!"
+        }
     }
-    const response = await fetch(
-        `${import.meta.env.VITE_BASEURL}:${import.meta.env.VITE_PORT}/clients/${body.id}`,
-        requestOptions
-    )
-    return response.json()
 }
