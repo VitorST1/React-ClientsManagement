92de7b14a67873fac371da6bea95c6098121e91b
Vitor <vitorturqueti@hotmail.com>
2024-02-01 09:28:47 -0300

    on server initialization, create indexes if they dont exist

--- Backend/src/config/database.ts
+++ Backend/src/config/database.ts
@@ -9,16 +9,31 @@ async function initializeDatabase() {
     console.log("Connecting to DB")
 
     // Cria a tabela de clientes caso ainda não exista
-    await pool.query(`
-        CREATE TABLE IF NOT EXISTS clients (
-            id SERIAL PRIMARY KEY,
-            name VARCHAR(100) NOT NULL,
-            email VARCHAR(100),
-            phone VARCHAR(15),
-            coordinatex NUMERIC NOT NULL,
-            coordinatey NUMERIC NOT NULL
-        )
-    `)
+    try {
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS clients (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(100) NOT NULL,
+                email VARCHAR(100),
+                phone VARCHAR(15),
+                coordinatex NUMERIC NOT NULL,
+                coordinatey NUMERIC NOT NULL
+            )
+        `)
+
+        // Cria os indexes dos campos name, email e phone caso ainda não existam
+        await pool.query(`
+            CREATE INDEX IF NOT EXISTS idx_clients_name ON clients (name);
+        `)
+        await pool.query(`
+            CREATE INDEX IF NOT EXISTS idx_clients_email ON clients (email);
+        `)
+        await pool.query(`
+            CREATE INDEX IF NOT EXISTS idx_clients_phone ON clients (phone);
+        `)
+    } catch (error) {
+        console.error(error)
+    }
 }
 
 pool.on("connect", () => {
